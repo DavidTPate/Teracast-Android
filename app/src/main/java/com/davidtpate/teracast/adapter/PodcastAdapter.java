4c59105ef0a3e1ff2f304fff51d81f1e54ea0fae
David Pate <davidtpate@gmail.com>
2014-05-18 14:09:29 -0400

    Add .gradle to ignore.

---
+++ app/src/main/java/com/davidtpate/teracast/adapter/PodcastAdapter.java
@@ -0,0 +1,6 @@
+package com.davidtpate.teracast.adapter;
+
+/**
+ * Created by David on 5/18/2014.
+ */
+public class PodcastAdapter {}
---
+++ app/src/main/java/com/davidtpate/teracast/model/PodcastList.java
@@ -0,0 +1,6 @@
+package com.davidtpate.teracast.model;
+
+/**
+ * Created by David on 5/18/2014.
+ */
+public class PodcastList {}
